b6efee188cddbf12e0109270cb4526cbe65507e4
irisahah <irisharjo@gmail.com>
2024-03-05 17:05:04 +0200

    Initial commit

---
+++ react_harjutus1/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Iris' Favorite Book</h1>
+      <h2>Don't Make Me Think (Revisited) by Steve Krug, 2013</h2>
+    </div>
+  );
+}
+
+export default App;
