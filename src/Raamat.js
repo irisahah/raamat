4df7b07312663bb2663c31321ac411244be57d41
irisahah <irisharjo@gmail.com>
2024-03-11 13:32:42 +0200

    up

--- src/Raamat.js
+++ src/Raamat.js
@@ -1,4 +1,4 @@
-import image from './raamat.png';
+import image from './raamat 1.png';
 
 function Raamat() {
     return (
@@ -9,6 +9,7 @@ function Raamat() {
             <p>Since Don’t Make Me Think was first published in 2000, hundreds of thousands of Web designers and developers have relied on usability guru Steve Krug’s guide to help them understand the principles of intuitive navigation and information design. Witty, commonsensical, and eminently practical, it’s one of the best-loved and most recommended books on the subject.</p>
             <p>Now Steve returns with fresh perspective to reexamine the principles that made Don’t Make Me Think a classic–with updated examples and a new chapter on mobile usability. And it’s still short, profusely illustrated…and best of all–fun to read.</p>
             <p>If you’ve read it before, you’ll rediscover what made Don’t Make Me Think so essential to Web designers and developers around the world. If you’ve never read it, you’ll see why so many people have said it should be required reading for anyone working on Web sites.</p>
+            <button>View on Amazon</button>
         </div>
     )
 }
