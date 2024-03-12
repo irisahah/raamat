606fe5f3d14dc9d1a9be3615529ec6d2da9e70b0
irisahah <irisharjo@gmail.com>
2024-03-12 09:49:56 +0200

    up

--- src/App.js
+++ src/App.js
@@ -11,12 +11,12 @@ function App() {
          <h1>TOP BOOKS</h1>
       </header>
       <div className="book-list">
-        <Raamat title="Don't Make Me Think (Revisited)" author="Steve Krug" image={image1} />
-        <Raamat title="The 7 Habits of Highly Effective People" author="Stephen Covey " image={image2} />
-        <Raamat title="The Cat in the Hat" author="Dr. Seuss" image={image3} />
+        <Raamat title="Don't Make Me Think (Revisited)" author="Steve Krug" image={image1} link="https://www.amazon.com/Dont-Make-Think-Revisited-Usability/dp/0321965515" />
+        <Raamat title="The 7 Habits of Highly Effective People" author="Stephen Covey" image={image2} link="https://www.amazon.com/Habits-Highly-Effective-People-Powerful/dp/1982137274" />
+        <Raamat title="The Cat in the Hat" author="Dr. Seuss" image={image3} link="https://www.amazon.com/Cat-Hat-Dr-Seuss/dp/039480001X" />
       </div>
     </div>
-  )
+  );
 }
 
 export default App;
--- src/Raamat.js
+++ src/Raamat.js
@@ -1,13 +1,20 @@
 function Raamat(props) {
+    const handleButtonClick = () => {
+        window.open(props.link, '_blank');
+    };
+
     return (
         <div className="book">
-            <img src={props.image} className="book-image" />
+            <img src={props.image} className="book-image" alt="Book" />
             <div className="book-title">
-                <p><h2>Title: {props.title}</h2></p>
-                <p className="book-author"><h3>Author: {props.author}</h3></p>
+                <h2>Title: {props.title}</h2>
+                <h3 className="book-author">Author: {props.author}</h3>
+            </div>
+            <div>
+                <button onClick={handleButtonClick}>View on Amazon</button>
             </div>
         </div>
-    )
+    );
 }
 
 export default Raamat;
